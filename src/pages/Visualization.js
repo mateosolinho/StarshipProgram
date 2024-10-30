eb0563255ab19a842e2ee4f2a70dc7a41fbde14a
Mateo Soliño Blanco <mateosolino1@gmail.com>
2024-10-30 12:35:54 +0100

    Añadidos botones de muestreo de gráficos

--- src/pages/Visualization.js
+++ src/pages/Visualization.js
@@ -7,6 +7,7 @@ import Slider from 'react-slick';
 
 const Visualization = ({ configs }) => {
   const [data, setData] = useState([]);
+  const [selectedChart, setSelectedChart] = useState('general'); // Estado para el botón seleccionado
 
   useEffect(() => {
     const fetchData = async () => {
@@ -50,6 +51,14 @@ const Visualization = ({ configs }) => {
     arrows: true,
   };
 
+  const handleButtonClick = (chartType) => {
+    setSelectedChart(chartType);
+  };
+
+  const filteredData = selectedChart === 'general' 
+    ? data 
+    : data.filter((_, index) => configs[index].chartTitle.includes(selectedChart));
+
   return (
     <section id="visualization" className="p-10 w-full">
       <h2 className="text-4xl font-bold mb-4 text-center">Visualización de Datos</h2>
@@ -57,9 +66,24 @@ const Visualization = ({ configs }) => {
         Nuestros datos de telemetría se visualizan a través de gráficos interactivos, facilitando la interpretación de resultados.
       </p>
 
-      {data.length > 0 ? (
+      {/* Botones de selección */}
+      <div className="flex justify-center mb-8 space-x-4">
+        {['IFT1', 'IFT2', 'IFT3', 'IFT4', 'IFT5', 'general'].map((chartType) => (
+          <button
+            key={chartType}
+            onClick={() => handleButtonClick(chartType)}
+            className={`px-4 py-2 rounded-md font-semibold ${
+              selectedChart === chartType ? 'bg-blue-500 text-white' : 'bg-gray-300 text-gray-700'
+            } hover:bg-blue-400`}
+          >
+            {chartType.toUpperCase()}
+          </button>
+        ))}
+      </div>
+
+      {filteredData.length > 0 ? (
         <Slider {...sliderSettings}>
-          {data.map((chartData, index) => (
+          {filteredData.map((chartData, index) => (
             <div key={index} className="px-4">
               <h3 className="text-2xl mb-4 text-center">{configs[index].chartTitle}</h3>
               <ResponsiveContainer width="100%" height={400}>
